87b35452f4b27e99fe240eaaf35f0f7f0a8ff3a8
Mohammed Asrar Ahmed <mohammedasrar.ahmed04@gmail.com>
2025-06-17 15:28:49 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test{
+	public static void main (String args[]) {
+		int rno=101;
+		String name ="Asrar ahmed";
+		String dept="CSE";
+		System.out.println("student name is :"+name);
+		System.out.println("student roll is :"+rno);
+		System.out.println("student dept is :"+dept);
+	}
+}
